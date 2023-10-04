9727003448786b06bd5116c2159edfbe838694ea
emile2600 <emilemetry@outlook.com>
2023-10-04 17:01:37 -0400

    quick fix

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -9,17 +9,9 @@ using Scheduler.Data.Validators.Abstract;
 
 var builder = WebApplication.CreateBuilder(args);
 
-if (builder.Environment.IsProduction())
-{
-    var connectionString = builder.Configuration.GetConnectionString("AZURE-SQL-CONNECTIONSTRING");
-    var options = new SqlServerOptions() { ConnectionString = connectionString };
-    builder.Services.AddDbContext<ScheduleDb>(_ => ScheduleDb.Create(options));
-}
-else
-{
-    var options = new SqliteOptions() { DbName = "ScheduleDb" };
-    builder.Services.AddDbContext<ScheduleDb>(_ => ScheduleDb.Create(options));
-}
+var connectionString = builder.Configuration.GetConnectionString("AZURE-SQL-CONNECTIONSTRING");
+var options = new SqlServerOptions() { ConnectionString = connectionString };
+builder.Services.AddDbContext<ScheduleDb>(_ => ScheduleDb.Create(options));
 
 
 // Add services to the container.
