14d12e4d419030978d6316812e335d7f0fd70583
Emile <emilemetry@outlook.com>
2023-11-24 16:11:59 -0400

    display of the reservations

--- Scheduler/Page/src/pages/room/index.tsx
+++ Scheduler/Page/src/pages/room/index.tsx
@@ -9,6 +9,7 @@ import RoomType from "../../models/RoomType";
 import { useParams } from "react-router-dom";
 import Reservation from "../../models/Reservation";
 import { isSameDay } from "../../extensions/Date";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 
 const _info = {name: "Rooms", icon: <MdBedroomParent/>};
 
@@ -20,44 +21,50 @@ function RoomIndexBody(): ReactElement {
     const [state, setState] = useState(PageState.Default);
     let groupedRooms = groupByRoomType(rooms);
 
-
-    function Tables({rooms}: {rooms: Room[]}): ReactElement{
-        if(rooms.length === 0) return(<></>);
-
-        return(<>
-            <table className="table table-borderless mt-3">
-                <thead>
-                    <tr>
-                        <th style={{borderRadius: "5px 0px 0px 0px"}} colSpan={2} className="pb-0">
-                            {RoomType[Number(rooms[0].type)]}
-                        </th>
-                    </tr>
-                </thead> 
-                <tbody>
-                    <tr>
-                        <td className="flipped" style={{fontSize:"16px"}}>
-                            {_info.icon}
-                        </td>                            
-                        <Dates monthYear={monthYear}/>   
-                    </tr> 
-                    {rooms.map((r, index) => 
-                    <tr className="darken-on-hover" key={index}>
-                        <td className="flipped text-center">{r.number}</td>
-                        <RoomAvailibilty monthYear={monthYear} reservations={r.reservations ?? []}/>
-                    </tr>)}
-                </tbody>
-            </table>
-        </>);
+    function onMonthYearSelected(monthYear: Date): void {
+        setMonthYear(monthYear);
     }
 
     return(<>
-            <div className="p-3 pe-3 d-flex flex-column flex-fill">
-                {Object.keys(groupedRooms).map((key, index) => <Tables key={index} rooms={groupedRooms[parseInt(key)]}/>)}
-            </div>
+        <div style={{borderRadius:"5px"}} className="p-3 m-3 mb-2 bg-primary d-flex flex-fill flex-row">
+            <MonthYearSelector monthYear={monthYear} onChange={onMonthYearSelected}/>
+        </div>
+        <div className="p-3 d-flex flex-column flex-fill">
+            {Object.keys(groupedRooms).map((key, index) => <Tables key={index} monthYear={monthYear} showDates={index === 0} rooms={groupedRooms[parseInt(key)]}/>)}
+        </div>
     </>);
 }
 
 // #region Elements
+
+function Tables({rooms, monthYear, showDates}: {rooms: Room[], monthYear: Date, showDates: boolean}): ReactElement{
+    if(rooms.length === 0) return(<></>);
+
+    return(<>
+        <table className="table table-borderless mt-0 mb-0">
+            <thead >
+                <tr hidden={!showDates}>
+                    <td  className="flipped" style={{fontSize:"16px", borderRadius: "5px 0px 0px 0px"}}>
+                        {_info.icon}
+                    </td>                            
+                    <Dates monthYear={monthYear}/>   
+                </tr> 
+                <tr>
+                    <th colSpan={2} className="pb-0 pt-0 align-middle">
+                        {RoomType[Number(rooms[0].type)]}
+                    </th>
+                </tr>
+            </thead> 
+            <tbody>
+                {rooms.map((r, index) => 
+                <tr style={{overflow:"visible"}} className="darken-on-hover" key={index}>
+                    <td className="flipped text-center">{r.number}</td>
+                    <RoomAvailibilty monthYear={monthYear} reservations={r.reservations ?? []}/>
+                </tr>)}
+            </tbody>
+        </table>
+    </>);
+}
 
 function RoomAvailibilty({monthYear, reservations}: {monthYear: Date, reservations: Reservation[]}): ReactElement {
     const date = new Date(monthYear.getFullYear(), monthYear.getMonth() + 1, 0).getDate();
@@ -65,9 +72,7 @@ function RoomAvailibilty({monthYear, reservations}: {monthYear: Date, reservatio
         const date = new Date(monthYear.getFullYear(), monthYear.getMonth(), day);
         const checkIn: Reservation | undefined = reservations.find(r => isSameDay(date, r.checkIn!));
         const checkOut: Reservation | undefined = reservations.find(r => isSameDay(date, r.checkOut!));
-        const occupied: boolean = undefined !== reservations.find(r =>
-            (r.checkIn! < date) &&
-            (r.checkOut! > date));
+        const occupied: Reservation | undefined =  reservations.find(r => (r.checkIn! < date) && (r.checkOut! > date));
 
         function Cells(): ReactElement {
             enum CellType {Occupied, CheckIn, CheckOut, None}
@@ -82,7 +87,7 @@ function RoomAvailibilty({monthYear, reservations}: {monthYear: Date, reservatio
             }
 
             function Cell({type, left}: {type: CellType, left: boolean}): ReactElement {
-                
+
                 function getStyle(): string {
                     switch(type) {
                         case CellType.CheckIn: return("check-in");
@@ -93,14 +98,12 @@ function RoomAvailibilty({monthYear, reservations}: {monthYear: Date, reservatio
                 }
 
                 return(<>
-                    <div className={"flex-fill " + getStyle() + (left ? " left " : " right ")}>
-                    
-                    </div>
+                    <div className={"flex-fill " + getStyle() + (left ? " left " : " right ")}></div>
                 </>);
             }
 
             return(<>
-                <td style={{overflow: "hidden"}} className={"p-0 d-flex flex-fill border-dark border-1"}>
+                <td style={{overflow:"visible"}} className={"p-0 d-flex flex-fill border-dark border-1"}>
                     <Cell left={true} type={type1}/>
                     <Cell left={false} type={type2}/>
                 </td>
@@ -142,9 +145,24 @@ function Dates({monthYear}: {monthYear: Date}): ReactElement {
         for(let i = 1; i <= date; i++) days.push(<Day key={i} day={i}/>);
 
     return(<>
-        <tr className="d-flex bg-primary p-0">
+        <tr className="d-flex p-0">
             {days}
         </tr>
+    </>);
+}
+
+function MonthYearSelector({monthYear, onChange}: {monthYear: Date, onChange: (monthYear: Date) => void}): ReactElement {
+    const arrowClass = "btn btn-secondary me-2 justify-content-center";
+    return(<>
+        <div className="d-flex flex-fill flex-row justify-content-center align-items-center">
+            <button className={arrowClass} onClick={() => onChange(new Date(monthYear.getFullYear(), monthYear.getMonth() - 1, 1))}>
+                <FaLongArrowAltLeft />
+            </button>
+            <h5 className="text-center text-secondary me-2 ms-2">{monthYear.toLocaleString('default', { month: 'long' }) + " " + monthYear.getFullYear()}</h5>
+            <button className={arrowClass} onClick={() => onChange(new Date(monthYear.getFullYear(), monthYear.getMonth() + 1, 1))}>
+                <FaLongArrowAltRight />
+            </button>
+        </div>
     </>);
 }
 
